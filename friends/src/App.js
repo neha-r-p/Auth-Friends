2848fcfdbb298def859751bc9a214d0cfc7d67b4
Neha Patel <neha.r.p@gmail.com>
2019-08-20 15:24:36 -0700

    simple login form component created and routed to App.js and successfully rendering

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import Login from './components/Login';
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <div className="App">
+      <Link to="/login">Login</Link>
 
-    </div>
+      <Route path="/login" component={Login} />
+      </div>
+    </Router>
   );
 }
 
---
+++ friends/src/components/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <form>
+      <input type="text" />
+      <input type="password" />
+      <button>Submit</button>
+    </form>
+  );
+};
+
+export default Login;
