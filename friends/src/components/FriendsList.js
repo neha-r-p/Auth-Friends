5e43f9d27702a18ce2a3fa8b5bdde57d8f33fff5
Neha Patel <neha.r.p@gmail.com>
2019-08-20 16:04:21 -0700

    created PrivateRoute.js

---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Friend from "./Friend";
+
+const FriendsList = () => {
+  return (
+    <div>
+      List of Friends: <Friend />
+    </div>
+  );
+};
+
+export default FriendsList;
