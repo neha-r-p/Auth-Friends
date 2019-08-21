c36efc4978b5390c989f13b07cee79455cc1b7cf
Neha Patel <neha.r.p@gmail.com>
2019-08-20 18:03:25 -0700

    login working to get token after correct login

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from 'axios';
 
 const Login = () => {
   const [credentials, setCredentials] = useState({
@@ -10,8 +11,19 @@ const Login = () => {
     setCredentials({ ...credentials, [e.target.name]: e.target.value });
   };
 
+  const login = e => {
+    console.log("form submitted");
+    e.preventDefault();
+    axios
+      .post("http://localhost:5000/api/login", this.state.credentials)
+      .then(res => {
+        localStorage.setItem("token", res.data.payload);
+      })
+      .catch(err => console.log(err.response));
+  };
+
   return (
-    <form>
+    <form onSubmit={login}>
       <label>
         Username:{" "}
         <input
