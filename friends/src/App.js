673a1118225b12b19f459c442f0d840b75e906a6
Neha Patel <neha.r.p@gmail.com>
2019-08-20 22:39:36 -0700

    simple styling with scss

--- friends/src/App.js
+++ friends/src/App.js
@@ -12,11 +12,12 @@ function App() {
   return (
     <Router>
       <div className="App">
+        <div className="links">
       <Link to="/login">Login</Link>
       <Link to="/add-friend">Add Friend</Link>
       <Link to="/friends-list">List of Friends</Link>
       <Link to="/login" onClick={() => localStorage.clear()}>Logout</Link>
-
+</div>
       <Route path="/login" component={Login} />
       <PrivateRoute path="/add-friend" component={AddFriend} />
       <PrivateRoute exact path="/friends-list" component={FriendsList} />
