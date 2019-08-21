8a8721a2f8a932dbe7a8c86072a2e9be3e87da37
Neha Patel <neha.r.p@gmail.com>
2019-08-20 17:52:30 -0700

    added relevant attributes to input tags

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -7,16 +7,28 @@ const Login = () => {
   });
 
   const changeHandler = e => {
-      setCredentials({...credentials, [e.target.name]: e.target.value})
-  }
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+  };
 
   return (
     <form>
       <label>
-        Username: <input type="text" />
+        Username:{" "}
+        <input
+          type="text"
+          name="username"
+          value={credentials.username}
+          onChange={changeHandler}
+        />
       </label>
       <label>
-        Password: <input type="password" />
+        Password:{" "}
+        <input
+          type="password"
+          name="password"
+          value={credentials.password}
+          onChange={changeHandler}
+        />
       </label>
 
       <button>Submit</button>
