d585e2624389cd66b02be0c14f86b1991cd90b24
Neha Patel <neha.r.p@gmail.com>
2019-08-20 17:38:33 -0700

    added useState hook to FriendsList

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Friend from "./Friend";
 
 const FriendsList = () => {
+
+    const [ friendsList, setFriendsList ] = useState([ {
+        name: '',
+        age: '',
+        email: '',
+        id: '',
+    }])
+    
   return (
     <div>
       List of Friends: <Friend />
