60183a05a846d85326da5e606c50e272c9a6f6b7
Neha Patel <neha.r.p@gmail.com>
2019-08-21 14:11:40 -0700

    added now and some more styling

--- friends/src/components/Friend.js
+++ friends/src/components/Friend.js
@@ -1,12 +1,26 @@
 import React from "react";
 
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+  card: {
+    maxWidth: 345,
+    width: 345,
+    margin: '20px',
+    boxShadow: '0px 0px 3px 3px #dc651b'
+  }
+});
+
 const Friend = ({ friend }) => {
+    const classes = useStyles();
   return (
-    <div>
-      <h3>{friend.name}</h3>
+    <Card className={classes.card}>
+      <Typography variant="h4">{friend.name}</Typography>
       <p>Age: {friend.age}</p>
       <p>E-mail: {friend.email}</p>
-    </div>
+    </Card>
   );
 };
 
